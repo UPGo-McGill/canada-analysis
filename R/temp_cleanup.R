d711eee794ae143af4343679f95612a85da9651b
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-05-16 10:40:18 -0400

    Canada_daily trimming

--- R/temp_cleanup.R
+++ R/temp_cleanup.R
@@ -9,4 +9,7 @@ load("data/Canada_property.Rdata")
 
 
 
-Canada_daily
+Canada_daily <- Canada_daily %>% 
+  select(-ML, -day_num, -day_num_r, -lifelength, -FREH, -FREH2, -CMAgroup) %>% 
+  rename("R365" = R3652, "A365" = A3652, "ML" = MLnew)
+
