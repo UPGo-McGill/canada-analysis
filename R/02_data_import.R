d90150fdb82e13206f5479ad262a7aacde9b588d
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-05-16 12:57:03 -0400

    Data import cleaned up

--- R/02_data_import.R
+++ R/02_data_import.R
@@ -1,4 +1,17 @@
 ### DATA IMPORT
 
+## Load private data files
+
+load("data/Canada_daily.Rdata")
+load("data/Canada_property.Rdata")
+
+Canada_daily <- Canada_daily %>% 
+  select(-ML, -day_num, -day_num_r, -lifelength, -FREH, -FREH2, -CMAgroup) %>% 
+  rename("R365" = R3652, "A365" = A3652, "ML" = MLnew) %>% 
+  filter(Date <= "2018-12-31")
+
+
+# Load public data files
+
 csd_units <- get_census("CA16", regions = list(C = c("01")), level = 'CSD') %>% 
   select(GeoUID, Dwellings, Population)
