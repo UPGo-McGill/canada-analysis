dac153fc6b9117f8009e3d26218a8924a49272fb
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-05-16 11:03:39 -0400

    Script cleanup

--- R/temp_cleanup.R
+++ R/temp_cleanup.R
@@ -11,5 +11,6 @@ load("data/Canada_property.Rdata")
 
 Canada_daily <- Canada_daily %>% 
   select(-ML, -day_num, -day_num_r, -lifelength, -FREH, -FREH2, -CMAgroup) %>% 
-  rename("R365" = R3652, "A365" = A3652, "ML" = MLnew)
+  rename("R365" = R3652, "A365" = A3652, "ML" = MLnew) %>% 
+  filter(Date <= "2018-12-31")
 
