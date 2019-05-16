33e5b40c503c2f90d2d35d7661ed97d4270e6295
jennifercombs <jennifercombs814@gmail.com>
2019-05-16 11:19:40 -0400

    Figure 3 added

--- R/figures.R
+++ R/figures.R
@@ -60,6 +60,134 @@ rm(Fig2_data, Fig2_col, Fig2_panel_a, Fig2_panel_b)
 
 ## Figure 3 ####
 
+da <- st_read("data", "lda_000a16a_e")
+st_geometry(da) <- NULL
+da$DAUID <- as.numeric(as.character(da$DAUID))
+
+Canada_property <- Canada_property %>% 
+  left_join(da[,c(1,9)], by = c("winner" = "DAUID"))
+
+
+
+##Graph---------------------------------------------------------------------------------------------------------------------
+graph <- Canada_daily %>% 
+  filter(CMATYPE == "Rural",
+         Date >= "2018-01-01",
+         Date <= "2018-12-31",
+         Housing == TRUE) %>% 
+  group_by(Property_ID) %>% 
+  summarise(n = n(), Revenue = sum(Price[Status == "R"]))
+
+freh <- Canada_FREH %>% 
+  filter(Date == "2018-12-31", FREH ==1) %>% 
+  select(Property_ID, FREH)
+
+
+
+graph3 <- Canada_daily %>% 
+  filter(CMATYPE == "Rural",
+         Date >= "2018-01-01",
+         Date <= "2018-12-31",
+         Housing == TRUE)
+
+graph3 <- graph3 %>% 
+  left_join(Canada_property[,c(1,20,21,22,30)], by = "Property_ID") 
+
+FREH_2018_2 <- FREH_2018 %>% 
+  filter(FREH == 1)
+
+graph3 <- graph3 %>% 
+  left_join(FREH_2018_2[,c(1,3)], by = "Property_ID") 
+
+graph4 <- graph3 %>% 
+  group_by(Date, CSDNAME, CSDUID) %>% 
+  summarize(n = n(), rev = sum(Price[Status == "R"])) %>% 
+  group_by(CSDNAME, CSDUID) %>% 
+  summarise(ADL = mean(n), rev = sum(rev))
+
+csd_units2 <- get_census("CA16", regions = list(C = c("01")), level = 'CSD')
+csd_units2 <- csd_units2 %>% 
+  select(GeoUID, Dwellings, Population)
+
+graph5 <- graph4 %>% 
+  group_by(CSDUID, CSDNAME) %>% 
+  left_join(csd_units2, by = c("CSDUID" = "GeoUID")) %>% 
+  mutate(Outlier = ifelse(CSDNAME %in% c("Whistler", 
+                                         "Mont-Tremblant", 
+                                         "Prince Edward County",
+                                         "The Blue Mountains"), 1, 0)) 
+#mutate(percentFREH = FREH.y/ADL)
+
+graph5 <- graph5 %>% 
+  mutate(revCAD = 1.2957*rev)
+
+
+graph5 %>% 
+  #filter(percentFREH <=1) %>% 
+  ggplot(aes(x = Dwellings, y = ADL))+
+  # geom_mark_hull(aes(x = Dwellings, 
+  #  y = ADL, 
+  #  filter = Outlier == 1,
+  # fill = Outlier),
+  # alpha = 0.25,
+  # color = "#9ebcda",
+  #fill = "#9ebcda",
+  #radius = unit(5, "mm"),
+  # concavity = 2,
+  #expand = unit(5, "mm"),
+  # show.legend = FALSE)+
+geom_point(color = "#0c316b", alpha = 0.5)+
+  annotate("text", x = 12950, y = 2063, label = "Whistler", size = 2.5)+
+  annotate("text", x = 16000, y = 1000, label = "Prince Edward County", size = 2.5)+
+  annotate("text", x = 12300, y = 713, label = "Mont-Tremblant", size = 2.5)+
+  annotate("text", x = 11450, y = 561, label = "The Blue Mountains", size = 2.5)+
+  geom_smooth(se = FALSE, color = "black")+
+  xlab("Housing units")+
+  ylab("Listngs")+
+  #scale_size_continuous(name = "Percentage of \nFREH listings", labels = scales::percent_format(accuracy = .1))+
+  scale_x_continuous(labels = scales::comma_format(accuracy = 1), limits = c(0,20500))+
+  scale_y_continuous(labels = scales::comma_format(accuracy = 1), limits = c(0,2200))+
+  theme(panel.grid.major.x = element_line(size = 0.05, color = "grey80"),
+        text=element_text(size=10),
+        axis.text = element_text(size = 10),
+        panel.grid.major.y = element_line(size = 0.05, color = "grey80"),
+        panel.grid.minor.y = element_line(size = 0.025, color = "grey80"),
+        legend.key = element_blank(),
+        panel.background=element_blank(),
+        axis.line = element_line(size = .09, color = "grey10"))
+
+graph5 %>% 
+  ggplot(aes(x = ADL, y = revCAD))+
+  #geom_mark_hull(aes(x = ADL, y = revCAD, 
+  # filter = Outlier == 1,
+  # fill = Outlier),
+  #alpha = 0.25,
+  #color = "#9ebcda",
+  #fill = "#9ebcda",
+  #radius = unit(5, "mm"),
+  # concavity = 2,
+  #expand = unit(5, "mm"),
+  #show.legend = FALSE)+
+  geom_point(color = "#0c316b", alpha = 0.5)+
+  annotate("text", x = 1800, y = 87500000, label = "Whistler", size = 2.5)+
+  annotate("text", x = 1500, y = 24000000, label = "Prince Edward County", size = 2.5)+
+  annotate("text", x = 1150, y = 18000000, label = "Mont-Tremblant", size = 2.5)+
+  annotate("text", x = 400, y = 30000000, label = "The Blue Mountains", size = 2.5)+
+  geom_smooth(se = FALSE, color = "black")+
+  xlab("Listings")+
+  ylab("Revenue")+
+  scale_size_continuous(name = "Percentage of \nFREH listings", labels = scales::percent_format(accuracy = .1))+
+  theme(panel.grid.major.x = element_line(size = 0.05, color = "grey80"),
+        text=element_text(size=10),
+        axis.text = element_text(size = 10),
+        panel.grid.major.y = element_line(size = 0.05, color = "grey80"),
+        panel.grid.minor.y = element_line(size = 0.025, color = "grey80"),
+        legend.key = element_blank(),
+        panel.background=element_blank(),
+        axis.line = element_line(size = .09, color = "grey10"))+
+  scale_x_continuous(labels = scales::comma_format(accuracy = 1),limits = c(0, 2250))+
+  scale_y_continuous(labels = scales::dollar_format(accuracy = 1),limits = c(0, 95000000))
+
 
 
 
